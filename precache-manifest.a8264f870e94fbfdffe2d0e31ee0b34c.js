89c9fa069ef1d16bd38b1507bf00a921e233ffc8
ETBlue <etblue.tw@gmail.com>
2018-11-08 03:50:57 +0800

    Updates

---
+++ precache-manifest.a8264f870e94fbfdffe2d0e31ee0b34c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbaa34b92334a798fc96dd712617aa98",
+    "url": "/game/static/media/citizenergy_game_data - plant.bbaa34b9.csv"
+  },
+  {
+    "revision": "f8d07f135a799d03fc7e",
+    "url": "/game/static/css/main.915aec19.chunk.css"
+  },
+  {
+    "revision": "fa8c47e04c8fea666470",
+    "url": "/game/static/js/1.fa8c47e0.chunk.js"
+  },
+  {
+    "revision": "dffb2a4fcbf3ea7182df",
+    "url": "/game/static/js/runtime~main.dffb2a4f.js"
+  },
+  {
+    "revision": "5dd64e3b72445790d7da7abb40843ddd",
+    "url": "/game/static/media/welcome.5dd64e3b.png"
+  },
+  {
+    "revision": "286f44dbe8812fb783c1f8bc6acfcb8a",
+    "url": "/game/static/media/main_power_consumer-answer.286f44db.png"
+  },
+  {
+    "revision": "4f02e943bbe57337ce26ddcb20624a40",
+    "url": "/game/static/media/peak_time-answer.4f02e943.png"
+  },
+  {
+    "revision": "06af125d466cd6c9ebd34689519f510f",
+    "url": "/game/static/media/taiwan_number_one-answer.06af125d.png"
+  },
+  {
+    "revision": "e7944b1ee6193e3df1356c0fd9378b92",
+    "url": "/game/static/media/citizenergy_game_data - city.e7944b1e.csv"
+  },
+  {
+    "revision": "1e6706ee6a493cddb9bd2bc29ae36ed2",
+    "url": "/game/static/media/citizenergy_game_data - category.1e6706ee.csv"
+  },
+  {
+    "revision": "1efd311bed1607d40aac18a5d9c3122d",
+    "url": "/game/static/media/citizenergy_game_data - energy.1efd311b.csv"
+  },
+  {
+    "revision": "f8d07f135a799d03fc7e",
+    "url": "/game/static/js/main.f8d07f13.chunk.js"
+  },
+  {
+    "revision": "6ce704cc3a32128d1c17b6a0fe0dcf89",
+    "url": "/game/static/media/banner.6ce704cc.png"
+  },
+  {
+    "revision": "91710b87676647d6910d7cc3108768c3",
+    "url": "/game/static/media/city_or_country.91710b87.png"
+  },
+  {
+    "revision": "83eeeed1d16d28b294c33d112d54f29d",
+    "url": "/game/static/media/main_power_consumer.83eeeed1.png"
+  },
+  {
+    "revision": "1e86375d2449e37871ce87465e0633ee",
+    "url": "/game/static/media/participatory_policy.1e86375d.png"
+  },
+  {
+    "revision": "1a896bb70cf171436cc8a6f7da4fc2c4",
+    "url": "/game/static/media/peak_time.1a896bb7.png"
+  },
+  {
+    "revision": "3c59ec7dc86f9ff033395b4b2d6b4c93",
+    "url": "/game/static/media/power_plant.3c59ec7d.png"
+  },
+  {
+    "revision": "cc5f4a403eafc50b72d4c631bb978787",
+    "url": "/game/static/media/power_saving_policy.cc5f4a40.png"
+  },
+  {
+    "revision": "cd446f763bb1587c55b751e0e79e7a6d",
+    "url": "/game/static/media/renewable_energy_policy.cd446f76.png"
+  },
+  {
+    "revision": "c90532c283b9439fe2e87bff0e308445",
+    "url": "/game/static/media/taiwan_number_one.c90532c2.png"
+  },
+  {
+    "revision": "c4915a0469c6bae72f8553ddc184b470",
+    "url": "/game/index.html"
+  }
+];
